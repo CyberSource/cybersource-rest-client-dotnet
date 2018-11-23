4c56ed6c2855600b187ea130bffae86dc0ef5c46
akshunyaInfy <akshunya_bharti@infosys.com>
2018-11-23 12:52:18 +0530

    ApiRequest Properties Setters Changed to Private

--- Client/ApiRequest.cs
+++ Client/ApiRequest.cs
@@ -15,13 +15,13 @@ namespace CyberSource.Client
         /// Gets or sets the HTTP headers
         /// </summary>
         /// <value>HTTP headers</value>
-        public IList<string> Headers { get; set; }
+        public IList<string> Headers { get; private set; }
 
         /// <summary>
         /// Gets or sets the data (parsed HTTP Request body)
         /// </summary>
         /// <value>The data.</value>
-        public T Data { get;  set; }
+        public T Data { get; private set; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ApiRequest&lt;T&gt;" /> class.
