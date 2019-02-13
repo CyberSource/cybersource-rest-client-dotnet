2da7cafedb2c333d10116dd42a557752cd32f425
akshunyaInfy <akshunya_bharti@infosys.com>
2019-02-13 12:05:42 +0530

    change path in powershell commands

--- generator/cybersource_csharp_sdk_gen.bat
+++ generator/cybersource_csharp_sdk_gen.bat
@@ -1,23 +1,23 @@
 java -jar swagger-codegen-cli-2.2.3.jar generate -t cybersource-csharp-template -i cybersource-rest-spec.json -l csharp -o ../ -c cybersource-csharp-config.json
 
-powershell -Command "(Get-Content .\Csharp\src\CyberSource\Api\CreditApi.cs) | ForEach-Object { $_ -replace '/pts/v2/credits/', '/pts/v2/credits' } | Set-Content .\Csharp\src\CyberSource\Api\CreditApi.cs"
+powershell -Command "(Get-Content ..\src\CyberSource\Api\CreditApi.cs) | ForEach-Object { $_ -replace '/pts/v2/credits/', '/pts/v2/credits' } | Set-Content ..\src\CyberSource\Api\CreditApi.cs"
 
-powershell -Command "(Get-Content .\Csharp\src\CyberSource\Api\PaymentsApi.cs) | ForEach-Object { $_ -replace '/pts/v2/payments/', '/pts/v2/payments' } | Set-Content .\Csharp\src\CyberSource\Api\PaymentsApi.cs"
+powershell -Command "(Get-Content ..\src\CyberSource\Api\PaymentsApi.cs) | ForEach-Object { $_ -replace '/pts/v2/payments/', '/pts/v2/payments' } | Set-Content ..\src\CyberSource\Api\PaymentsApi.cs"
 
-powershell -Command "(Get-Content .\Csharp\src\CyberSource\Api\ProcessAPayoutApi.cs) | ForEach-Object { $_ -replace '/pts/v2/payouts/', '/pts/v2/payouts' } | Set-Content .\Csharp\src\CyberSource\Api\ProcessAPayoutApi.cs"
+powershell -Command "(Get-Content ..\src\CyberSource\Api\ProcessAPayoutApi.cs) | ForEach-Object { $_ -replace '/pts/v2/payouts/', '/pts/v2/payouts' } | Set-Content ..\src\CyberSource\Api\ProcessAPayoutApi.cs"
 
-powershell -Command "(Get-Content .\Csharp\src\CyberSource\Api\ReportDownloadsApi.cs) | ForEach-Object { $_ -replace '/reporting/v3/report-downloads', '/reporting/v3/report-downloads/?reportDate={reportDate}&reportName={reportName}' } | Set-Content .\Csharp\src\CyberSource\Api\ReportDownloadsApi.cs"
+powershell -Command "(Get-Content ..\src\CyberSource\Api\ReportDownloadsApi.cs) | ForEach-Object { $_ -replace '/reporting/v3/report-downloads', '/reporting/v3/report-downloads/?reportDate={reportDate}&reportName={reportName}' } | Set-Content ..\src\CyberSource\Api\ReportDownloadsApi.cs"
 
-powershell -Command "(Get-Content .\Csharp\src\CyberSource\Api\ReportDownloadsApi.cs) | ForEach-Object { $_ -replace 'if \(organizationId != null\)', '//if (organizationId != null)' } | Set-Content .\Csharp\src\CyberSource\Api\ReportDownloadsApi.cs"
+powershell -Command "(Get-Content ..\src\CyberSource\Api\ReportDownloadsApi.cs) | ForEach-Object { $_ -replace 'if \(organizationId != null\)', '//if (organizationId != null)' } | Set-Content ..\src\CyberSource\Api\ReportDownloadsApi.cs"
 
-powershell -Command "(Get-Content .\Csharp\src\CyberSource\Api\ReportDownloadsApi.cs) | ForEach-Object { $_ -replace 'if \(reportDate != null\)', '//if (reportDate != null)' } | Set-Content .\Csharp\src\CyberSource\Api\ReportDownloadsApi.cs"
+powershell -Command "(Get-Content ..\src\CyberSource\Api\ReportDownloadsApi.cs) | ForEach-Object { $_ -replace 'if \(reportDate != null\)', '//if (reportDate != null)' } | Set-Content ..\src\CyberSource\Api\ReportDownloadsApi.cs"
 
-powershell -Command "(Get-Content .\Csharp\src\CyberSource\Api\ReportDownloadsApi.cs) | ForEach-Object { $_ -replace 'if \(reportName != null\)', '//if (reportName != null)' } | Set-Content .\Csharp\src\CyberSource\Api\ReportDownloadsApi.cs"
+powershell -Command "(Get-Content ..\src\CyberSource\Api\ReportDownloadsApi.cs) | ForEach-Object { $_ -replace 'if \(reportName != null\)', '//if (reportName != null)' } | Set-Content ..\src\CyberSource\Api\ReportDownloadsApi.cs"
 
-powershell -Command "(Get-Content .\Csharp\src\CyberSource\Api\TransactionBatchesApi.cs) | ForEach-Object { $_ -replace '/pts/v1/transaction-batches', '/pts/v1/transaction-batches?startTime={startTime}&endTime={endTime}' } | Set-Content .\Csharp\src\CyberSource\Api\TransactionBatchesApi.cs"
+powershell -Command "(Get-Content ..\src\CyberSource\Api\TransactionBatchesApi.cs) | ForEach-Object { $_ -replace '/pts/v1/transaction-batches', '/pts/v1/transaction-batches?startTime={startTime}&endTime={endTime}' } | Set-Content ..\src\CyberSource\Api\TransactionBatchesApi.cs"
 
-powershell -Command "(Get-Content .\Csharp\src\CyberSource\Api\TransactionBatchesApi.cs) | ForEach-Object { $_ -replace 'if \(startTime != null\)', '//if (startTime != null)' } | Set-Content .\Csharp\src\CyberSource\Api\TransactionBatchesApi.cs"
+powershell -Command "(Get-Content ..\src\CyberSource\Api\TransactionBatchesApi.cs) | ForEach-Object { $_ -replace 'if \(startTime != null\)', '//if (startTime != null)' } | Set-Content ..\src\CyberSource\Api\TransactionBatchesApi.cs"
 
-powershell -Command "(Get-Content .\Csharp\src\CyberSource\Api\TransactionBatchesApi.cs) | ForEach-Object { $_ -replace 'if \(endTime != null\)', '//if (endTime != null)' } | Set-Content .\Csharp\src\CyberSource\Api\TransactionBatchesApi.cs"
+powershell -Command "(Get-Content ..\src\CyberSource\Api\TransactionBatchesApi.cs) | ForEach-Object { $_ -replace 'if \(endTime != null\)', '//if (endTime != null)' } | Set-Content ..\src\CyberSource\Api\TransactionBatchesApi.cs"
 
 pause
