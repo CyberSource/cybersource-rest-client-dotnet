fa643c00dd081d377b0cc5346a839faceafd255a
karthikeyan kumar <karthkum@visa.com>
2021-08-02 13:11:05 +0530

    adding code changes to TransientToken.cs

---
+++ Utilities/Flex/Model/TransientTokenGenerator/TransientTokenGenerator.cs
@@ -0,0 +1,81 @@
+namespace CyberSource.Utilities.Flex.Model.TransientTokenGenerator
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class TransientTokenGenerator
+    {
+        [JsonProperty("flx")]
+        public Flx Flx { get; set; }
+
+        [JsonProperty("ctx")]
+        public Ctx[] Ctx { get; set; }
+
+        [JsonProperty("iss")]
+        public string Iss { get; set; }
+
+        [JsonProperty("exp")]
+        public long Exp { get; set; }
+
+        [JsonProperty("iat")]
+        public long Iat { get; set; }
+
+        [JsonProperty("jti")]
+        public string Jti { get; set; }
+    }
+
+    public partial class Ctx
+    {
+        [JsonProperty("data")]
+        public Data Data { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+
+    public partial class Data
+    {
+        [JsonProperty("targetOrigins")]
+        public Uri[] TargetOrigins { get; set; }
+
+        [JsonProperty("mfOrigin")]
+        public Uri MfOrigin { get; set; }
+    }
+
+    public partial class Flx
+    {
+        [JsonProperty("path")]
+        public string Path { get; set; }
+
+        [JsonProperty("data")]
+        public string Data { get; set; }
+
+        [JsonProperty("origin")]
+        public Uri Origin { get; set; }
+
+        [JsonProperty("jwk")]
+        public Jwk Jwk { get; set; }
+    }
+
+    public partial class Jwk
+    {
+        [JsonProperty("kty")]
+        public string Kty { get; set; }
+
+        [JsonProperty("e")]
+        public string E { get; set; }
+
+        [JsonProperty("use")]
+        public string Use { get; set; }
+
+        [JsonProperty("n")]
+        public string N { get; set; }
+
+        [JsonProperty("kid")]
+        public string Kid { get; set; }
+    }
+}
---
+++ Utilities/Flex/Utilities/TransientToken.cs
@@ -0,0 +1,35 @@
+
+namespace CyberSource.Utilities.Flex.Model.Utilities
+{
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+    using Newtonsoft.Json.JsonSerializer;
+    using CyberSource.Utilities.Flex.Model.TransientTokenGenerator.TransientTokenGenerator;
+    public class TransientTokenGenerator {
+   
+    /**
+     * This is the method to extract transient token from given jwt token 
+     * @param jwt
+     * @return
+     */
+    public var extractTransientToken(string jwt)
+    {
+        // split the jwt token into two parts 
+        // bearer and token part
+        string splitContents[] = jwt.Split(".");
+        if(splitContents.length > 1)    
+        {
+            // decode the base 64 encoded string
+            String encodedString  = splitContents[1];
+            byte[] data = Convert.FromBase64String(encodedString);
+            string decodedString = Encoding.UTF8.GetString(encodedString);
+            Newtonsoft.Json.JsonSerializer s = new JsonSerializer();
+            var transientTokenModel= s.Deserialize<TransientTokenGenerator>(new JsonTextReader(new StringReader(decodedString)));
+            // return JTI string
+            return transientTokenModel;
+        }
+      
+        return null;
+    }
+}
+}
