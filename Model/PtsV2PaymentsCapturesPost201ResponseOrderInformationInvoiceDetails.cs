92b0e8eb6ce9f0cdd592854d8cea909cf89097ab
kmauzoul <58862511+kmauzoul@users.noreply.github.com>
2023-09-13 13:44:45 -0400

    Level3TransmissionStatus Deserialization fix on CaptureResponse
    The Level3TransmissionStatus is a string (not bool?) - this causes deserialization errors when Capturing payments with Level 3 data (PurchaseLevel set to "3") -
    
    This will correct this issue -

--- Model/PtsV2PaymentsCapturesPost201ResponseOrderInformationInvoiceDetails.cs
+++ Model/PtsV2PaymentsCapturesPost201ResponseOrderInformationInvoiceDetails.cs
@@ -34,7 +34,7 @@ namespace CyberSource.Model
         /// Initializes a new instance of the <see cref="PtsV2PaymentsCapturesPost201ResponseOrderInformationInvoiceDetails" /> class.
         /// </summary>
         /// <param name="Level3TransmissionStatus">Indicates whether CyberSource sent the Level III information to the processor. The possible values are:  If your account is not enabled for Level III data or if you did not include the purchasing level field in your request, CyberSource does not include the Level III data in the request sent to the processor.  Possible values: - **true** - **false** .</param>
-        public PtsV2PaymentsCapturesPost201ResponseOrderInformationInvoiceDetails(bool? Level3TransmissionStatus = default(bool?))
+        public PtsV2PaymentsCapturesPost201ResponseOrderInformationInvoiceDetails(string Level3TransmissionStatus = default(string))
         {
             this.Level3TransmissionStatus = Level3TransmissionStatus;
         }
@@ -44,7 +44,7 @@ namespace CyberSource.Model
         /// </summary>
         /// <value>Indicates whether CyberSource sent the Level III information to the processor. The possible values are:  If your account is not enabled for Level III data or if you did not include the purchasing level field in your request, CyberSource does not include the Level III data in the request sent to the processor.  Possible values: - **true** - **false** </value>
         [DataMember(Name="level3TransmissionStatus", EmitDefaultValue=false)]
-        public bool? Level3TransmissionStatus { get; set; }
+        public string Level3TransmissionStatus { get; set; }
 
         /// <summary>
         /// Returns the string presentation of the object
