efadaf689879a1be9a7565e06a8f47aaa236f4c5
kmauzoul <132375627+SRKMauzoul@users.noreply.github.com>
2023-09-13 14:15:26 -0400

    Adjust comments to reflect actual values
    Fix to comment to reflect whats actually returned from CyberSource ("Y" or "N")

--- Model/PtsV2PaymentsCapturesPost201ResponseOrderInformationInvoiceDetails.cs
+++ Model/PtsV2PaymentsCapturesPost201ResponseOrderInformationInvoiceDetails.cs
@@ -40,9 +40,9 @@ namespace CyberSource.Model
         }
         
         /// <summary>
-        /// Indicates whether CyberSource sent the Level III information to the processor. The possible values are:  If your account is not enabled for Level III data or if you did not include the purchasing level field in your request, CyberSource does not include the Level III data in the request sent to the processor.  Possible values: - **true** - **false** 
+        /// Indicates whether CyberSource sent the Level III information to the processor. The possible values are:  If your account is not enabled for Level III data or if you did not include the purchasing level field in your request, CyberSource does not include the Level III data in the request sent to the processor.  Possible values: - **Y** - **N** 
         /// </summary>
-        /// <value>Indicates whether CyberSource sent the Level III information to the processor. The possible values are:  If your account is not enabled for Level III data or if you did not include the purchasing level field in your request, CyberSource does not include the Level III data in the request sent to the processor.  Possible values: - **true** - **false** </value>
+        /// <value>Indicates whether CyberSource sent the Level III information to the processor. The possible values are:  If your account is not enabled for Level III data or if you did not include the purchasing level field in your request, CyberSource does not include the Level III data in the request sent to the processor.  Possible values: - **Y** - **N** </value>
         [DataMember(Name="level3TransmissionStatus", EmitDefaultValue=false)]
         public string Level3TransmissionStatus { get; set; }
 
