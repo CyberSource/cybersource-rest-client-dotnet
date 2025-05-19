da11e79fead952b3480249ccd22abbf995306167
gaubansa <gaubansa@visa.com>
2025-05-19 14:52:57 +0530

    re-arranging the bat file

--- generator/cybersource_dotnet_sdk_gen.bat
+++ generator/cybersource_dotnet_sdk_gen.bat
@@ -13,24 +13,32 @@ endlocal
 
 java -jar swagger-codegen-cli-2.4.38.jar generate -t cybersource-csharp-template -i cybersource-rest-spec-dotnet.json -l csharp -o ../ -c cybersource-csharp-config.json
 
-powershell -Command "Get-ChildItem '..\src\Cybersource\Api\*.cs' -Recurse | ForEach-Object { (Get-Content $_).Replace('Method.POST','Method.Post').Replace('Method.GET','Method.Get').Replace('Method.PATCH','Method.Patch').Replace('Method.DELETE','Method.Delete').Replace('Method.PUT','Method.Put') | Set-Content $_ }"
+xcopy ..\src\cybersource ..\ /s /e /y /exclude:excludelist.txt
+md ..\test
+xcopy ..\src\cybersource.test ..\test /s /e /y
+rd /s /q ..\src
 
-powershell -Command "(Get-Content ..\src\CyberSource\Api\SecureFileShareApi.cs) | ForEach-Object { $_ -replace 'null\); \/\/ Return statement', 'localVarResponse.Content); // Return statement' } | Set-Content ..\src\CyberSource\Api\SecureFileShareApi.cs"
+echo "starting of powershell commands"
 
-powershell -Command "(Get-Content ..\src\CyberSource\Api\ReportDownloadsApi.cs) | ForEach-Object { $_ -replace 'null\); \/\/ Return statement', 'localVarResponse.Content); // Return statement' } | Set-Content ..\src\CyberSource\Api\ReportDownloadsApi.cs"
+powershell -Command "Get-ChildItem '..\Api\*.cs' -Recurse | ForEach-Object { (Get-Content $_).Replace('Method.POST','Method.Post').Replace('Method.GET','Method.Get').Replace('Method.PATCH','Method.Patch').Replace('Method.DELETE','Method.Delete').Replace('Method.PUT','Method.Put') | Set-Content $_ }"
 
-powershell -Command "(Get-Content ..\src\CyberSource\Api\TransactionBatchesApi.cs) | ForEach-Object { $_ -replace 'null\); \/\/ Return statement', 'localVarResponse.Content); // Return statement' } | Set-Content ..\src\CyberSource\Api\TransactionBatchesApi.cs"
+powershell -Command "(Get-Content ..\Api\SecureFileShareApi.cs) | ForEach-Object { $_ -replace 'null\); \/\/ Return statement', 'localVarResponse.Content); // Return statement' } | Set-Content ..\Api\SecureFileShareApi.cs"
 
-powershell -Command " Set-Content ..\src\CyberSource\Api\SecureFileShareApi.cs ((get-content ..\src\CyberSource\Api\SecureFileShareApi.cs -raw) -replace '\*_\/_\*;charset=utf-8', '*/*;charset=utf-8') "
+powershell -Command "(Get-Content ..\Api\ReportDownloadsApi.cs) | ForEach-Object { $_ -replace 'null\); \/\/ Return statement', 'localVarResponse.Content); // Return statement' } | Set-Content ..\Api\ReportDownloadsApi.cs"
+
+powershell -Command "(Get-Content ..\Api\TransactionBatchesApi.cs) | ForEach-Object { $_ -replace 'null\); \/\/ Return statement', 'localVarResponse.Content); // Return statement' } | Set-Content ..\Api\TransactionBatchesApi.cs"
+
+powershell -Command " Set-Content ..\Api\SecureFileShareApi.cs ((get-content ..\Api\SecureFileShareApi.cs -raw) -replace '\*_\/_\*;charset=utf-8', '*/*;charset=utf-8') "
 
 REM For changing nuspec filename in csproj file
-powershell -Command "(Get-Content ..\src\CyberSource\CyberSource.csproj) | ForEach-Object { $_ -replace '<None Include=\"CyberSource.nuspec\" />', '<None Include=\"cybersource-rest-client-dotnet.nuspec\" />' } | Set-Content ..\src\CyberSource\CyberSource.csproj"
+powershell -Command "(Get-Content ..\CyberSource.csproj) | ForEach-Object { $_ -replace '<None Include=\"CyberSource.nuspec\" />', '<None Include=\"cybersource-rest-client-dotnet.nuspec\" />' } | Set-Content ..\CyberSource.csproj"
 
-powershell -Command "(Get-Content ..\src\CyberSource.Test\CyberSource.Test.csproj) | ForEach-Object { $_ -replace 'CyberSource\\CyberSource.csproj', 'cybersource-rest-client-dotnet.csproj' } | ForEach-Object { $_ -replace '<Name>CyberSource</Name>', '<Name>cybersource-rest-client-dotnet</Name>' } | Set-Content ..\src\CyberSource.Test\CyberSource.Test.csproj"
+powershell -Command "(Get-Content ..\test\CyberSource.Test.csproj) | ForEach-Object { $_ -replace 'CyberSource\\CyberSource.csproj', 'cybersource-rest-client-dotnet.csproj' } | ForEach-Object { $_ -replace '<Name>CyberSource</Name>', '<Name>cybersource-rest-client-dotnet</Name>' } | Set-Content ..\test\CyberSource.Test.csproj"
 
 REM For Renaming the .csproj file Name from Cybersource to cybersource-rest-client-dotnet
-powershell Rename-Item ..\src\CyberSource\CyberSource.csproj cybersource-rest-client-dotnet.csproj
-powershell Rename-Item ..\src\CyberSource.Test\CyberSource.Test.csproj cybersource-rest-client-dotnet.Test.csproj
+del ..\cybersource-rest-client-dotnet.csproj
+powershell Rename-Item ..\CyberSource.csproj cybersource-rest-client-dotnet.csproj
+powershell Rename-Item ..\test\CyberSource.Test.csproj cybersource-rest-client-dotnet.Test.csproj
 
 REM For changing project filenames in sln file
 powershell -Command "(Get-Content ..\CyberSource.sln) | ForEach-Object { $_ -replace 'CyberSource', 'cybersource-rest-client-dotnet' } | Set-Content ..\CyberSource.sln"
@@ -40,7 +48,7 @@ del ..\cybersource-rest-client-dotnet.sln
 
 powershell Rename-Item ..\CyberSource.sln cybersource-rest-client-dotnet.sln
 
-xcopy ..\src\cybersource ..\ /s /e /y /exclude:excludelist.txt
+echo "completed powershell commands"
 
 @REM replace sdkLinks fieldName to links for supporting links field name in request/response body
 echo "starting of replacing the links keyword in PblPaymentLinksAllGet200Response.cs model"
@@ -51,11 +59,8 @@ git checkout ..\README.md
 git checkout ..\Api\OAuthApi.cs
 git checkout ..\Model\AccessTokenResponse.cs
 git checkout ..\Model\CreateAccessTokenRequest.cs
-md ..\test
-xcopy ..\src\cybersource.test ..\test /s /e /y
-git checkout ..\test\packages.config
 
-rd /s /q ..\src
+git checkout ..\test\packages.config
 
 del ..\Client\IReadableConfiguration.cs
 
